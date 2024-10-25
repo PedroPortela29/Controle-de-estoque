57dae49549796657a5786b285f133789f9c8161c
PedroHAPortela <phaporte@gmail.com>
2024-10-25 12:06:50 -0300

    primeiro commit do trabalho da faculdade...
    configurando os arquivos

---
+++ Controle-de-EstoqueWEB/Controllers/GraficoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controle_de_EstoqueWEB.Controllers
+{
+    public class GraficoController : Controller
+    {
+        public IActionResult PerdaMes()
+        {
+            return View();
+        }
+        public IActionResult EntradaSaidaMes()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controle-de-EstoqueWEB/Controllers/RelatorioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controle_de_EstoqueWEB.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        public IActionResult PosicaoEstoque()
+        {
+            return View();
+        }
+        public IActionResult Ressuprimento()
+        {
+            return View();
+        }
+    }
+}
